738eacd3b376deb63e2bc0714888a8ece19c4fe7
Olakunle Salami <kunsal2003@gmail.com>
2020-11-14 04:29:58 +0100

    Updates

---
+++ precache-manifest.f17e820b8e230ffa3f8ea087ef413a0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "259f21b1fa5910ee873a90e85ceb8545",
+    "url": "/bikerace/index.html"
+  },
+  {
+    "revision": "f5c08d686bb817092746",
+    "url": "/bikerace/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "6b29889bc5531d59443c",
+    "url": "/bikerace/static/css/main.6ddd395e.chunk.css"
+  },
+  {
+    "revision": "f5c08d686bb817092746",
+    "url": "/bikerace/static/js/2.2bcd2b9f.chunk.js"
+  },
+  {
+    "revision": "876e403427d7be82fb4e8ac24123aaf3",
+    "url": "/bikerace/static/js/2.2bcd2b9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b29889bc5531d59443c",
+    "url": "/bikerace/static/js/main.33c08206.chunk.js"
+  },
+  {
+    "revision": "f6be73450d9610646e15",
+    "url": "/bikerace/static/js/runtime-main.6c781a3f.js"
+  }
+]);
